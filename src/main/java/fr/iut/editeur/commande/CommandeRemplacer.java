8af076a31d7312fad65e47039579c4558e7034ab
capo <mathys.capo@etu.umontpellier.fr>
2023-11-07 16:51:10 +0100

    remplacer qui marche

---
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -0,0 +1,23 @@
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+
+public class CommandeRemplacer extends CommandeDocument {
+
+
+    public CommandeRemplacer(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer() {
+        if(parameters.length < 2) {
+            System.err.println("Format attendu :  remplacer;depart;fin;chaine");
+            return;
+        }
+        String texte = parameters[1];
+        this.document.remplacer(Integer.parseInt(parameters[1]),Integer.parseInt(parameters[2]),parameters[3]);
+        super.executer();
+    }
+
+}
