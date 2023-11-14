323f6acd6ce7430759c23d523a7b83c67576f82f
capo <mathys.capo@etu.umontpellier.fr>
2023-11-14 16:47:28 +0100

    documentation

--- src/main/java/fr/iut/editeur/commande/Commande.java
+++ src/main/java/fr/iut/editeur/commande/Commande.java
@@ -1,6 +1,12 @@
 package fr.iut.editeur.commande;
 public interface Commande {
-
+    /**
+     * Description de la commande
+     */
     void getDescriptionCommande();
+
+    /**
+     * Commande a executer
+     */
     void executer();
 }
--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -25,25 +25,49 @@ public class Document {
         return this.contentDocument;
     }
 
+    /**
+     * @param start
+     * @param end
+     * @param remplacement
+     */
     public void remplacer(int start, int end, String remplacement) {
         String leftPart = contentDocument.substring(0, start);
         String rightPart = contentDocument.substring(end);
         contentDocument = leftPart + remplacement + rightPart;
     }
+
+    /**
+     * @param start
+     * @param end
+     */
     public void majuscules(int start, int end) {
         String leftPart = contentDocument.substring(start,end);
         remplacer(start,end,leftPart.toUpperCase());
     }
+
+    /**
+     * @param start
+     * @param end
+     */
     public void effacer(int start, int end) {
         remplacer(start,end,"");
-    }public void clear() {
+    }
+    public void clear() {
         contentDocument="";
     }
 
+    /**
+     * @param start
+     * @param texte
+     */
     public void inserer(int start,String texte){
         remplacer(start,start,texte);
     }
 
+    /**
+     * @param start
+     * @param end
+     */
     public void minuscules(int start, int end) {
         String leftPart = contentDocument.substring(start,end);
         remplacer(start,end,leftPart.toLowerCase());
